5b7791a924f3d2b735bf5a61ef83fb679601b63d
romanov1589 <35072304+romanov1589@users.noreply.github.com>
2018-01-14 20:47:18 +0100

    change an app behavior with spring-boot configuration

--- src/main/java/com/example/bookingdemo/ViewController.java
+++ src/main/java/com/example/bookingdemo/ViewController.java
@@ -1,10 +1,14 @@
 package com.example.bookingdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.swing.text.View;
 import java.util.Date;
 
 /**
@@ -12,6 +16,13 @@ import java.util.Date;
  */
 @Controller
 public class ViewController {
+
+    private String appMode;
+
+    @Autowired
+    public ViewController(Environment environment){
+        this.appMode = environment.getProperty("app-mode");
+    }
     /**
      * GET an index page
      * @param model add attributes for our index page
@@ -21,7 +32,7 @@ public class ViewController {
     public String index(Model model){
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Igor Romanov");
-        model.addAttribute("mode", "development");
+        model.addAttribute("mode", appMode);
         return "index";
     }
 }
